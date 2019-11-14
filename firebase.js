b83c65200b427ce68347d0ac4e9f40dc328c1d9a
Enrique <eabucam@gmail.com>
2019-11-14 11:25:24 -0500

    up

---
+++ firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCZmaSdOSRPIuCRNhyPCgS5LoQHO-SlBOk",
+  authDomain: "fritangas-c82e6.firebaseapp.com",
+  databaseURL: "https://fritangas-c82e6.firebaseio.com",
+  projectId: "fritangas-c82e6",
+  storageBucket: "fritangas-c82e6.appspot.com",
+  messagingSenderId: "964180946238",
+  appId: "1:964180946238:web:6e9558374750315754106f"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
